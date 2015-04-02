9be950c5e0876faecd42d7d98da343c9c7e47e46
Nathan Wallace <techietotoro@gmail.com>
2015-04-02 11:39:12 -0700

    add email contact option

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
 var mouseoverSocialIcon = function(obj) {
 	var currentColor = obj.getElementsByTagName("path")[0].getAttribute("fill");
-	obj.getElementsByTagName("path")[0].setAttribute("fill", "#ffffff");
+	var things = obj.getElementsByTagName("path");
+	for (var i = things.length - 1; i >= 0; i--) {
+		things[i].setAttribute("fill", "#FFFFFF");
+	};
 	obj.style.backgroundColor = currentColor;
 }
 
 var resetSocialIcon = function(obj) {
 	var currentColor = obj.getElementsByTagName("path")[0].getAttribute("fill");
-	obj.getElementsByTagName("path")[0].setAttribute("fill", obj.style.backgroundColor);
+	var things = obj.getElementsByTagName("path");
+	for (var i = things.length - 1; i >= 0; i--) {
+		things[i].setAttribute("fill", obj.style.backgroundColor);
+	};
 	obj.style.backgroundColor = currentColor;
 }
