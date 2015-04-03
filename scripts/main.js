e7e4a0f65f47f88a77d18606d322dda9a44515b5
Nathan Wallace <techietotoro@gmail.com>
2015-04-03 01:41:13 -0700

    change social links to yaml-based liquid stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 var mouseoverSocialIcon = function(obj) {
-	var currentColor = obj.getElementsByTagName("path")[0].style.fill;
-	var things = obj.getElementsByTagName("path");
+	var doc = obj.getElementsByTagName("object")[0].getSVGDocument();
+
+	var things = doc.getElementsByTagName("path");
+	var currentColor = things[0].style.fill;
 	for (var i = things.length - 1; i >= 0; i--) {
 		things[i].style.fill = "#FFFFFF";
 	};
@@ -8,10 +10,14 @@ var mouseoverSocialIcon = function(obj) {
 }
 
 var resetSocialIcon = function(obj) {
-	var currentColor = obj.getElementsByTagName("path")[0].style.fill;
-	var things = obj.getElementsByTagName("path");
+	var doc = obj.getElementsByTagName("object")[0].getSVGDocument();
+
+	var things = doc.getElementsByTagName("path");
+	var currentColor = things[0].style.fill;
 	for (var i = things.length - 1; i >= 0; i--) {
 		things[i].style.fill = obj.style.backgroundColor;
 	};
 	obj.style.backgroundColor = currentColor;
 }
+
+
