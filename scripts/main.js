1efc583d621c9b006c8ad9ba770a57f4ccde4aba
Nathan Wallace <techietotoro@gmail.com>
2015-04-02 20:34:46 -0700

    make some social icon changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
 var mouseoverSocialIcon = function(obj) {
-	var currentColor = obj.getElementsByTagName("path")[0].getAttribute("fill");
+	var currentColor = obj.getElementsByTagName("path")[0].style.fill;
 	var things = obj.getElementsByTagName("path");
 	for (var i = things.length - 1; i >= 0; i--) {
-		things[i].setAttribute("fill", "#FFFFFF");
+		things[i].style.fill = "#FFFFFF";
 	};
 	obj.style.backgroundColor = currentColor;
 }
 
 var resetSocialIcon = function(obj) {
-	var currentColor = obj.getElementsByTagName("path")[0].getAttribute("fill");
+	var currentColor = obj.getElementsByTagName("path")[0].style.fill;
 	var things = obj.getElementsByTagName("path");
 	for (var i = things.length - 1; i >= 0; i--) {
-		things[i].setAttribute("fill", obj.style.backgroundColor);
+		things[i].style.fill = obj.style.backgroundColor;
 	};
 	obj.style.backgroundColor = currentColor;
 }
